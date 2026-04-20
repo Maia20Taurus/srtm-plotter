a3b171ad3b56fca4111534fea8c52636d1c0b8a2
Maia <maiataurus314@gmail.com>
2026-04-20 14:38:18 +0100

    RasterPoint <-> Geopoint conversions added
    Added lerp_between_ranges for converting between RasterPoints and GeoPoints and vice versa
    
    convert_raster_to_geo and convert_geo_to_raster now implemented

--- src/grid.rs
+++ src/grid.rs
@@ -16,6 +16,7 @@ pub fn get_elevation_from_bounds(min_bound: &GeoPoint, max_bound: &GeoPoint) -> 
     let raster_height = (delta_latitude/RESOLUTION_DEGREES) as usize;
     let mut elevation_grid: Vec<Vec<i16>> = vec![vec![0; raster_width]; raster_height];
 
+    // The grid will be updated so this frame needs to be mutable
     let mut frame = SrtmFrame {
         min_bound: min_bound.clone(),
         max_bound: max_bound.clone(),
@@ -24,19 +25,52 @@ pub fn get_elevation_from_bounds(min_bound: &GeoPoint, max_bound: &GeoPoint) -> 
         grid: elevation_grid
     };
 
-    // Current task: Determine the dimensions for the grid (and then create it)
-
     // Next task: Enumerate each pixel, convert to GeoPoint and then Coord (from geo_types)
     // Then enumerate each tif file and use GeoTiff to find the file that contains the Coord
     // Then get the elevation for that point (this is not a good solution so this will be revisited later)
 }
 
+/// Use inverse linear interpolation to find the percentage of 'r1_value' between 'r1_start' and 'r1_end'
+/// and then use this to linearly interpolate between 'r2_start' and 'r2_end'
+fn lerp_between_ranges(r1_start: &f64, r1_end: &f64, r2_start: &f64, r2_end: &f64, r1_value: &f64) -> f64 {
+    let r1_percentage = (r1_value-r1_start)/(r1_end-r1_start);
+    r2_start + (r2_start-r2_end)*r1_percentage
+}
+
 /// Return a GeoPoint with the equivalent location of the provided RasterPoint
 pub fn convert_raster_to_geo(frame: &SrtmFrame, point: RasterPoint) -> GeoPoint {
-    todo!()
+    GeoPoint {
+        longitude: lerp_between_ranges(
+            &0.0,
+            &(frame.raster_width as f64),
+            &frame.min_bound.longitude,
+            &frame.max_bound.longitude,
+            &(point.x as f64)),
+        latitude: lerp_between_ranges(
+            &0.0,
+            &(frame.raster_height as f64),
+            &frame.min_bound.latitude,
+            &frame.max_bound.latitude,
+            &(point.y as f64))
+    }
 }
 
 /// Return a RasterPoint with the equivalent location of the provided GeoPoint
-pub fn convert_geo_to_raster(frame: &SrtmFrame, point: GeoPoint) -> RasterPoint {
-    todo!()
+pub fn convert_geo_to_raster(frame: &SrtmFrame, point: &GeoPoint) -> RasterPoint {
+    RasterPoint {
+        x: lerp_between_ranges(
+            &frame.min_bound.longitude,
+            &frame.max_bound.longitude,
+            &0.0,
+            &(frame.raster_width as f64),
+            &point.longitude) as usize,
+        y: lerp_between_ranges(
+            &frame.min_bound.latitude,
+            &frame.max_bound.latitude,
+            &0.0,
+            &(frame.raster_height as f64),
+            &point.latitude) as usize
+    }
 }
+
+// Current task: Write the conversion functions
