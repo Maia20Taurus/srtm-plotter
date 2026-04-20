4ac875140043b3ae4c833edc9c9825aa3d7a1133
Maia <maiataurus314@gmail.com>
2026-04-20 11:24:38 +0100

    GeoPoint moved into its own module; get_elevation_at_pixel's definition created

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,12 @@
 // Create a struct that contains elevation data for a requested rect with min and max bound
 // Determine the raster grid for this rect, then query each point to get elevation
 
-/// A pair of coordinates (in degrees) representing a point on Earth
-pub struct GeoPoint {
-    longitude: f64,
-    latitude: f64
-}
+mod geo;
+mod grid_construction;
 
-impl GeoPoint {
-    /// Create a new GeoPoint
-    pub fn new (longitude: f64, latitude: f64) -> Self {
-        GeoPoint { longitude, latitude }
-    }
-}
+use geo::GeoPoint;
 
+/// Contains elevation data for a specified boundary from the SRTM dataset
 pub struct SrtmFrame {
     // The bottom left and top right of the frame in degrees
     min_bound: GeoPoint,
@@ -30,11 +23,13 @@ impl SrtmFrame {
         todo!()
     }
 
-    pub fn get_elevation_at_pixel(latitude: u64, longitude: u64) -> i16 {
-        todo!()
+    /// Get the elevation at the specified pixel
+    pub fn get_elevation_at_pixel(&self, latitude: usize, longitude: usize) -> i16 {
+        self.grid[latitude][longitude]
     }
 
-    pub fn get_elevation_at_point(point: GeoPoint) -> i16 {
+    /// Get the elevation at the specified GeoPoint
+    pub fn get_elevation_at_point(&self, point: GeoPoint) -> i16 {
         todo!()
     }
 }
